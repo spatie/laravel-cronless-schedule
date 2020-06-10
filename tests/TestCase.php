fe4ce87035075930299e3fd12302012fff500bc3
freek <freek@spatie.be>
2020-06-10 09:51:36 +0200

    wip

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,10 +2,8 @@
 
 namespace Spatie\CronlessSchedule\Tests;
 
-use Illuminate\Contracts\Console\Kernel;
 use Orchestra\Testbench\TestCase as Orchestra;
 use Spatie\CronlessSchedule\CronlessScheduleServiceProvider;
-use Spatie\CronlessSchedule\Tests\TestClasses\TestKernel;
 
 class TestCase extends Orchestra
 {
@@ -14,10 +12,5 @@ class TestCase extends Orchestra
         return [
             CronlessScheduleServiceProvider::class,
         ];
-    }
-
-    protected function resolveApplicationConsoleKernel($app)
-    {
-        $app->singleton(Kernel::class, TestKernel::class);
     }
 }
